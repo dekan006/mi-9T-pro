0a4157b0e47309bdd57bc6b809c6fd11b1ee45fc
Denis Kanash <70863033+dekan006@users.noreply.github.com>
2021-07-14 14:10:53 +0300

    Update all.js

--- script/all.js
+++ script/all.js
@@ -11,5 +11,28 @@ jQuery(function($) {
             autoplaySpeed: 2000,
         });
       });
-  
+    $(document).ready(function(){
+        $('.slider-2').slick({
+            
+            slidesToScroll: 1,
+            autoplay: true,
+            dots: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                  breakpoint: 1920,
+                  settings: {
+                    slidesToShow: 3
+                  }
+                },
+                {
+                  breakpoint: 768,
+                  settings: {
+                    
+                    slidesToShow: 1
+                  }
+                }
+              ]
+        });
+      });
 })
